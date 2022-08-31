eefc5075bf4a475a6bf55773245e3ce257dd9bb6
wangbingtan <3095469149@qq.com>
2022-08-31 19:19:33 +0800

    second commit test/hello.java

--- src/test/java/com/conpany/project/hello.java
+++ src/test/java/com/conpany/project/hello.java
@@ -1,7 +1,14 @@
 package com.conpany.project;
 
 public class hello {
+    public static void func(int a){
+         a = 20;
+        System.out.println(a);
+       
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a=10;//实参
+        func(a);
+        System.out.println(a);
     }
 }
